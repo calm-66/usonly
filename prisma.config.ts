7425092f66285bd5964d8a61921142fc127701e6
Beite Xue <beite.xue@tu-dresden.de>
2026-03-24 10:26:13 +0800

    Fix: remove prisma.config.ts for Vercel deployment

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import path from 'node:path'
-import { PrismaConfig } from 'prisma'
-
-export default {
-  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
-  datasource: {
-    url: `file:${path.join(__dirname, 'prisma', 'dev.db')}`,
-  },
-} satisfies PrismaConfig
