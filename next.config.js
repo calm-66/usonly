f09709345c7783b47d7ec25287867d38441e79eb
Beite Xue <beite.xue@tu-dresden.de>
2026-03-30 17:00:06 +0800

    feat: 删除首页，根路径重定向到时间轴页面

--- next.config.js
+++ next.config.js
@@ -13,6 +13,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/timeline',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
